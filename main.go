70f180787cad8d4fccaa5fd96a43332758f64659
vequve <yazy@terrorists.store>
2024-05-03 09:57:06 +0200

    Added flags, improved error handling

--- main.go
+++ main.go
@@ -2,47 +2,41 @@ package main
 
 import (
     "fmt"
-    "io/ioutil"
+    "io"
     "encoding/json"
     "net/http"
-    "os"
-    "strconv"
+    "flag"
+    //"strconv"
 ) 
-
-const defN = 1
-const apiUrl ="https://api.quotable.io/random"
-
-
+const apiUrl = "https://api.quotable.io/random"
 type Quote struct {
+    id   string `json:""`
    Content string `json:"content"`
    Author string `json:"author"`
 }
 func main(){
-    
-    numN := defN
-    if(len(os.Args)>1){    
-        arg, err := strconv.Atoi(os.Args[1])
-       if err != nil{
-            fmt.Println(err)
-        }else{
-            numN = arg
-        }
+    numN := flag.Int("n", 1, "Numbers of quotes");
+    flag.Parse()
+    if *numN<1{
+        fmt.Println("Number of quotes needs to be positive! ")
+        
     }
-    for i:=0;i<numN;i++{
+    for i:=0;i<*numN;i++{
         quote()
     }
   
 }
 
 func quote(){
-       response, err := http.Get(apiUrl)
+    response, err := http.Get(apiUrl)
     
     if err != nil{
         fmt.Println(err)
         return
     }
-    defer response.Body.Close() 
-    body, err := ioutil.ReadAll(response.Body)  
+    defer response.Body.Close()
+   
+    body, err := io.ReadAll(response.Body)  
     if err != nil{
         fmt.Println(err)
         return
@@ -51,11 +45,10 @@ func quote(){
     var quotedata Quote
 
     err = json.Unmarshal(body, &quotedata)
-     if err != nil{
+    if err != nil{
         fmt.Println(err)
         return
     }
     fmt.Println(quotedata.Content)
-    fmt.Println("-",quotedata.Author)
-    
+    fmt.Println("-",quotedata.Author) 
 }
