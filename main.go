5e0d9f671dafa280465e136aead633ebf36d94ff
vequve <yazy@terrorists.store>
2024-05-04 18:07:15 +0200

    Fixed an issue with the index of the array of quotes

--- main.go
+++ main.go
@@ -14,34 +14,60 @@ type Quote struct{
     Tags []string `json:"tags"`
 
 }
+
 func main(){
     numN := flag.Int("n", 1, "Numbers of quotes");
     flag.Parse()
     numNs := strconv.Itoa(*numN);
     apiUrl :=  "https://api.quotable.io/quotes/random?limit="+numNs
     if *numN<1{
-        fmt.Println("Number of quotes needs to be positive! ")
-        
+        fmt.Println("Number of quotes needs to be positive! ") 
     }
-    quote(numN,apiUrl)    
-  
+    quote(numN,apiUrl)   
 }
 
 func quote(num *int, apiUrl string) error{
-    response, err := http.Get(apiUrl)
+    if *num > 50{
+        for i :=0;i<*num/50;i++{
+            response, err := http.Get(apiUrl)
     
-    if err != nil{
-        return err
-    }
-    defer response.Body.Close()
-    var quotedata []Quote 
+            if err != nil{
+                return err
+            }
+            defer response.Body.Close()
+            var quotedata []Quote 
     
-    if err := json.NewDecoder(response.Body).Decode(&quotedata); err!=nil{
-        return err
+            if err := json.NewDecoder(response.Body).Decode(&quotedata); err!=nil{
+                return err
+            }
+
+            for  j := 0;j<*num;j++{
+                fmt.Println(quotedata[j].Content)
+                fmt.Println("-",quotedata[j].Author)
+                if j>=49{
+                    break
+                }
+      
+            }
+        }
+    }else{
+        response, err := http.Get(apiUrl)
+    
+        if err != nil{
+            return err
+        }
+        defer response.Body.Close()
+        var quotedata []Quote 
+    
+        if err := json.NewDecoder(response.Body).Decode(&quotedata); err!=nil{
+            return err
+        }
+
+        for  j := 0;j<*num;j++{
+            fmt.Println(quotedata[j].Content)
+            fmt.Println("-",quotedata[j].Author)      
+        }
+
     }
-    for i := 0;i<*num;i++{
-        fmt.Println(quotedata[i].Content)
-        fmt.Println("-",quotedata[i].Author) 
-    } 
     return nil
 }
